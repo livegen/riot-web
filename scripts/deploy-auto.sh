6e5229a71175ec1665347ec1d0a02418eef84548
Smith <DarkSmith@users.noreply.github.com>
2019-02-21 11:46:47 +0100

    Update deploy-auto.sh

--- scripts/deploy-auto.sh
+++ scripts/deploy-auto.sh
@@ -9,6 +9,8 @@ npm run clean
 npm run build$dev
 
 cp config.json webapp/
+mkdir -p webapp/.well-known/matrix
+echo '{"m.server":"hs.livegen.net"}' > webapp/.well-known/matrix/server
 
 if [[ ${version} =~ ^v[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+(-.+)?$ ]]; then
     echo ${version:1} > webapp/version
